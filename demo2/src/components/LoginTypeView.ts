8493d995e52755e26281013e0a370fc3297e6006
yjtx <friendhhy@163.com>
2016-03-16 17:05:05 +0800

    add new demo

---
+++ demo2/src/components/LoginTypeView.ts
@@ -0,0 +1,43 @@
+/**
+ *
+ * @author
+ *
+ */
+class LoginTypeView extends egret.gui.SkinnableComponent {
+
+
+    public btnGroup:egret.gui.Group;
+
+    private loginType:nest.user.ILoginTypes;
+
+    public constructor(loginType:nest.user.ILoginTypes) {
+        super();
+
+        this.loginType = loginType;
+        this.skinName = skins.LoginTypeViewSkin;
+    }
+
+    public childrenCreated() {
+        super.childrenCreated();
+
+        var self = this;
+
+        for (var i:number = 0; i < this.loginType.loginTypes.length; i++) {
+            var logT:nest.user.ILoginType = this.loginType.loginTypes[i];
+
+            var url = "";
+            if (logT.accInfo && logT.accInfo.avatarUrl) {
+                url = logT.accInfo.avatarUrl;
+            }
+            var btn:LoginButtonView = new LoginButtonView(logT.loginType, url);
+            btn.name = logT.loginType;
+            this.btnGroup.addElement(btn);
+            btn.scaleX = btn.scaleY = 0.5;
+
+            btn.touchEnabled = true;
+            btn.addEventListener(egret.TouchEvent.TOUCH_TAP, function (e:egret.TouchEvent) {
+                self.loginType.onChoose(this.name);
+            }, btn);
+        }
+    }
+}
---
+++ demo2/src/components/LoginView.ts
@@ -0,0 +1,58 @@
+/**
+ *
+ * @author 
+ *
+ */
+class LoginView extends egret.gui.SkinnableComponent implements nest.user.ILoginCallbacks{
+	
+    
+    public info_txt: egret.gui.Label;
+    
+    public login_button: egret.gui.Button;
+    quickTest_btn: egret.gui.Button;
+    public constructor() {
+        super();
+        this.skinName = skins.LoginViewSkin;
+	}
+	
+	
+	public createChildren(){
+        super.createChildren();  
+        this.login_button.addEventListener(egret.TouchEvent.TOUCH_TAP,this.onTouchTapHandler,this);   
+        this.quickTest_btn.addEventListener(egret.TouchEvent.TOUCH_TAP,this.onQuickTestHandler,this);   
+	}
+	
+	private onQuickTestHandler(e:egret.TouchEvent):void {
+        utils.changeView(new QuickTestView());
+	}
+	
+	private onTouchTapHandler(e:egret.TouchEvent):void{
+
+        var self = this;
+        nest.core.startup({egretAppId : 88888, version : 2, debug:true}, function(resultInfo:nest.core.ResultCallbackInfo) {
+            if (resultInfo.result == 0) {
+                self.checkLogin();
+            }
+        });
+	}
+
+    public onCreate = (data: nest.user.ILoginTypes):void =>  {
+        utils.changeView(new LoginTypeView(data));
+    };
+
+    public onSuccess = (data: nest.user.LoginCallbackInfo):void => {
+        egret.log("log Success");
+        new Login().login(data);
+    };
+
+    public onFail = (data: nest.core.ResultCallbackInfo):void => {
+        egret.log("log Fail");
+    };
+
+	private checkLogin():void{
+
+        egret.log("checkLogin start");
+
+        nest.user.resLogin(this);
+	}
+}
